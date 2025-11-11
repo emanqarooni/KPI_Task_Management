91da5b012f22fe68e9f7edfa72bdc7998de06f42
emannn077 <emanrauf779@gmail.com>
2025-11-11 14:24:13 +0300

    profile and role based done

---
+++ kpi/main_app/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import EmployeeProfile
+
+
+@receiver(post_save, sender=User)
+def create_employee_profile(sender, instance, created, **kwargs):
+    if created:
+
+        EmployeeProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_employee_profile(sender, instance, **kwargs):
+
+    instance.employeeprofile.save()
