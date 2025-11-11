bcb8bba5af026be48262d615ff54ff2e0a65f6d4
Fatima-Zaid <fatimaameer957@gmail.com>
2025-11-11 15:19:33 +0300

    add main tag in base page

--- kpi/main_app/urls.py
+++ kpi/main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.home, name="home"),
+    path('kpis/', views.kpis_index , name='index'),
+
 ]
