e61b56dee2da97a671bd2824317ebbbba0b88ff1
emanqarooni <emanqaroonibahrain@gmail.com>
2025-11-11 11:22:34 +0300

    finished initial setup

---
+++ kpi/main_app/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+from datetime import date
+
+DEPARTMENT = (
+    ('SM', 'Sales & Marketing'),
+    ('IT', 'Information Technology'),
+    ('HR', 'Human Resources'),
+    ('PM', 'Project Management')
+)
+# Create your models here.
+class Kpi(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    department = models.CharField(max_length=1, choices=DEPARTMENT, default=DEPARTMENT[0][0])
+    # def __str__(self):
+    #     return f"{self.plushie.name} {self.get_method_display()} on {self.date}"
+
+class EmployeeKpi(models.Model):
+    kpi = models.ForeignKey(Kpi, on_delete=models.CASCADE)
+    target_value = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class ProgressEntry:
+    employee_kpi = models.ForeignKey(EmployeeKpi, on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    note = models.TextField(max_length=250)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
---
+++ kpi/main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import EmployeeKpi, ProgressEntry, Kpi
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# create your views here
+def home(request):
+    return render(request, 'home.html')
+
+
+
