5f3ec52b1ce296627df118092381b2686916d77e
emannn077 <emanrauf779@gmail.com>
2025-11-12 11:16:40 +0300

    created custom decorator and used in views.py

--- kpi/main_app/views.py
+++ kpi/main_app/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import EmployeeKpi, ProgressEntry, Kpi
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-
+from django.views.generic import CreateView, UpdateView
+from .decorators import RoleRequiredMixin
 # create your views here
 def home(request):
     return render(request, "home.html")
@@ -15,11 +15,23 @@ def kpis_index(request):
     return render(request, "kpi/index.html", {"kpis": kpis})
 
 
-def kpis_detail(request,kpi_id):
+def kpis_detail(request, kpi_id):
     kpi = Kpi.objects.get(id=kpi_id)
     return render(request, "kpi/detail.html", {"kpi": kpi})
 
+class KpiCreate(CreateView):
+    model = Kpi
+    fields = "__all__"
+    success_url="/kpis/"
+    allowed_role=['admin']
+    
+class KpiUpdate(UpdateView):
+    model = Kpi
+    fields = "__all__"
+    allowed_role=['admin']
 
-def kpis_index(request):
-    # kpi = Kpi.objects.get(id=kpi_id)
-    return render(request, "kpi/detail.html")
+
+# unauthorized access page
+def unauthorized(request):
+    return render(request,"unauthorized.html")
+
