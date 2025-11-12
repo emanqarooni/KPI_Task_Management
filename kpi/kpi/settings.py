9aa0786550716fa5b8994f92a99f3ab7e1a64397
emannn077 <emanrauf779@gmail.com>
2025-11-12 11:16:40 +0300

    created custom decorator and used in views.py

--- kpi/kpi/settings.py
+++ kpi/kpi/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+LOGIN_REDIRECT_URL="/kpis/"
+LOGOUT_REDIRECT_URL="/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- kpi/main_app/urls.py
+++ kpi/main_app/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path('kpis/', views.kpis_index , name='index'),
-    path('kpis/<int:kpi_id>/', views.kpis_detail , name='detail'),
-
+    path("kpis/", views.kpis_index, name="index"),
+    path("kpis/<int:kpi_id>/", views.kpis_detail, name="detail"),
+    path("kpis/create/", views.KpiCreate.as_view(), name="kpis_create"),
+    path("kpis/<int:pk>/update/", views.KpiUpdate.as_view(), name="kpis_update"),
+    path("unauthorized/", views.unauthorized, name="unauthorized"),
 ]
