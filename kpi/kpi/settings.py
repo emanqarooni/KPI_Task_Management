5b307c98b9ac3462881a270f52fea77793589fa3
shooqbinzaiman <shooqshowqi@gmail.com>
2025-11-12 10:40:19 +0300

    chartjs installation and add to setting

--- kpi/kpi/settings.py
+++ kpi/kpi/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "dashboard",
+    "chartjs",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
