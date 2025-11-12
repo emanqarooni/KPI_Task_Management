b21ecffbab079e27209ec8f9ae1996f45e04cd80
shooqbinzaiman <shooqshowqi@gmail.com>
2025-11-12 17:35:01 +0300

    Adding admin in view

--- kpi/main_app/views.py
+++ kpi/main_app/views.py
@@ -1,9 +1,58 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from .models import EmployeeKpi, ProgressEntry, Kpi
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import EmployeeProfile
+# EmployeeKpi
 
+# Employee dashboard'
+def dashboard(request):
+    profile = EmployeeProfile.objects.get(user=request.user)
+
+    if profile.role == "admin":
+        return admin_dashboard(request)
+    elif profile.role == "manager":
+        return manager_dashboard(request)
+    else:
+        return employee_dashboard(request)
+
+
+def admin_dashboard(request):
+    total_users = User.objects.count()
+    total_employees = EmployeeProfile.objects.filter(role="employee").count()
+
+    context = {
+        "total_users": total_users,
+        "total_employees": total_employees,
+    }
+    return render(request, "dashboards/admin_dashboard.html", context)
+    # kpis = EmployeeKpi.objects.all()
+    # return render(request, "dashboards/admin_dashboard.html", {
+    #     "profiles": profiles,
+    #     "kpis": kpis,
+
+
+
+    # team = profile.team_members.all()
+    # team_kpis = EmployeeKpi.objects.filter(employee__in=team)
+    # return render(request, "dashboards/manager_dashboard.html", {
+    #     "profile": profile,
+    #     "team": team,
+    #     "team_kpis": team_kpis,
+    # })
+
+
+    # profile = EmployeeProfile.objects.get(user=request.user)
+    # kpis = profile.assigned_kpis.select_related("kpi").all()
+    # return render(request, "dashboards/employee_dashboard.html", {
+    #     "profile": profile,
+    #     "kpis": kpis,
+    # })
+# Manager Dashboard
+
+# Admin Dashboard
 
 # create your views here
 def home(request):
