2fd2c4d62dde2a23edcd0ea4c0e4189b4a4809ad
emanqarooni <emanqaroonibahrain@gmail.com>
2025-11-13 09:46:15 +0300

    adding the list assing kpi in the urls and views

--- kpi/main_app/urls.py
+++ kpi/main_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 
     # kpi assignments urls from manager portal
     path('assign-kpi/', views.assign_kpi, name='assign_kpi'),
+    path('employee-kpis/', views.employee_kpi_list, name='employee_kpi_list'),
 ]
