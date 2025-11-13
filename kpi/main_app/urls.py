bd975510c66f0e22984904919a58c2f7f972230a
emanqarooni <emanqaroonibahrain@gmail.com>
2025-11-13 09:54:32 +0300

    added delete assign kpi html, url and in views

--- kpi/main_app/urls.py
+++ kpi/main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('assign-kpi/', views.assign_kpi, name='assign_kpi'),
     path('employee-kpis/', views.employee_kpi_list, name='employee_kpi_list'),
     path('employee-kpi/<int:pk>/edit/', views.employee_kpi_edit, name='employee_kpi_edit'),
+    path('employee-kpi/<int:pk>/delete/', views.employee_kpi_delete, name='employee_kpi_delete'),
 ]
