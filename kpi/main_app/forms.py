59d89fc57e4d93374ab99656b1136ddd79c3db08
emanqarooni <emanqaroonibahrain@gmail.com>
2025-11-13 09:44:33 +0300

    added logic in the employee kpi model for shoing status and progress

--- kpi/main_app/forms.py
+++ kpi/main_app/forms.py
@@ -1,3 +1,4 @@
+# main_app/forms.py
 from django import forms
 from .models import EmployeeKpi, EmployeeProfile, Kpi
 
@@ -5,16 +6,18 @@ class AssignKpiForm(forms.ModelForm):
     class Meta:
         model = EmployeeKpi
         fields = ['employee', 'kpi', 'target_value', 'weight', 'start_date', 'end_date']
+        widgets = {
+            'start_date': forms.DateInput(attrs={'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'type': 'date'}),
+        }
 
-    employee = forms.ModelChoiceField(
-        queryset=EmployeeProfile.objects.filter(role="employee"),
-        label="Choose Employee"
-    )
-
-    kpi = forms.ModelChoiceField(
-        queryset=Kpi.objects.all(),
-        label="Choose KPI"
-    )
-
-    start_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    end_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    def __init__(self, *args, user=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if user and hasattr(user, "employeeprofile") and user.employeeprofile.role == "manager":
+            dept = user.employeeprofile.department
+            self.fields['employee'].queryset = EmployeeProfile.objects.filter(department=dept, role='employee')
+            self.fields['kpi'].queryset = Kpi.objects.filter(department=dept)
+        else:
+            # default: show all employees who are role='employee' and all KPIs
+            self.fields['employee'].queryset = EmployeeProfile.objects.filter(role='employee')
+            self.fields['kpi'].queryset = Kpi.objects.all()
--- kpi/main_app/urls.py
+++ kpi/main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path('kpis/', views.kpis_index , name='index'),
+
+    # kpi assignments urls from manager portal
     path('assign-kpi/', views.assign_kpi, name='assign_kpi'),
 ]
