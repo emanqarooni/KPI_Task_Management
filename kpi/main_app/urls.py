15fe85b42d8f81605d5a39017879fee1059b82c7
emannn077 <emanrauf779@gmail.com>
2025-11-16 09:54:27 +0300

    added forget password

--- kpi/main_app/urls.py
+++ kpi/main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path('kpis/', views.kpis_index , name='index'),
+    path("kpis/", views.kpis_index, name="index"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("dashboard/admin/", views.admin_dashboard, name="admin_dashboard"),
     path("dashboard/manager/", views.manager_dashboard, name="manager_dashboard"),
@@ -15,12 +15,18 @@ urlpatterns = [
     path("profile/", views.profile, name="profile"),
     path("kpis/progress/", views.add_progress, name="progress"),
     path("kpis/employee/", views.employee_kpi, name="employee_kpi"),
-
-
     # kpi assignments urls from manager portal
-    path('assign-kpi/', views.assign_kpi, name='assign_kpi'),
-    path('employee-kpis/', views.employee_kpi_list, name='employee_kpi_list'),
-    path('employee-kpi/<int:pk>/edit/', views.employee_kpi_edit, name='employee_kpi_edit'),
-    path('employee-kpi/<int:pk>/delete/', views.employee_kpi_delete, name='employee_kpi_delete'),
-    path('employee-kpi/<int:pk>/', views.employee_kpi_detail, name='employee_kpi_detail'),
+    path("assign-kpi/", views.assign_kpi, name="assign_kpi"),
+    path("employee-kpis/", views.employee_kpi_list, name="employee_kpi_list"),
+    path(
+        "employee-kpi/<int:pk>/edit/", views.employee_kpi_edit, name="employee_kpi_edit"
+    ),
+    path(
+        "employee-kpi/<int:pk>/delete/",
+        views.employee_kpi_delete,
+        name="employee_kpi_delete",
+    ),
+    path(
+        "employee-kpi/<int:pk>/", views.employee_kpi_detail, name="employee_kpi_detail"
+    ),
 ]
